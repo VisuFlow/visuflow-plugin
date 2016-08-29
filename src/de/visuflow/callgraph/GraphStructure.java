ce027e73e7212d41ff6028ccc2e63fac3053d278
Zafar Syed <zafar.habeeb1989@gmail.com>
2016-08-29 12:37:50 +0200

    Call Graph generator added

---
+++ src/de/visuflow/callgraph/GraphStructure.java
@@ -0,0 +1,14 @@
+package de.visuflow.callgraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import soot.SootMethod;
+import soot.Unit;
+
+public class GraphStructure {
+	public static List<Node> listNodes = new ArrayList<Node>();
+	public static List<Edge> listEdges = new ArrayList<Edge>();
+
+}
--- src/de/visuflow/callgraph/IntraproceduralAnalysis.java
+++ src/de/visuflow/callgraph/IntraproceduralAnalysis.java
@@ -1,4 +1,4 @@
-package de.visuflow.ex2;
+package de.visuflow.callgraph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import de.visuflow.callgraph.*;
 import de.visuflow.reporting.IReporter;
 import heros.edgefunc.EdgeIdentity;
 import soot.Body;
@@ -20,14 +21,14 @@ import soot.tagkit.SourceLnPosTag;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 import soot.toolkits.graph.*;
-import de.visuflow.ex2.*;
 
 public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowAbstraction>> {
 	public int flowThroughCount = 0;
 	private final SootMethod method;
-	private final IReporter reporter;
+	//private final IReporter reporter;
 	static ExceptionalUnitGraph eg;
 	public static int nodeNumber=0;
+	public static int edgeNumber=0;
 	public static int nodeCount=0;
 	public static int edgeCount=0;
 	public static HashMap<Unit,Integer> nodesMap = new HashMap<>();
@@ -39,11 +40,11 @@ public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowA
 	public static List<Edge> listEdges = new ArrayList<>();
 	
 	
-	public IntraproceduralAnalysis(Body b, IReporter reporter, GraphStructure g) {
+	public IntraproceduralAnalysis(Body b,GraphStructure g) {
 		super(new ExceptionalUnitGraph(b));
 		
 		this.method = b.getMethod();
-		this.reporter = reporter;
+		
 		System.out.println(b);
 		Options.v().set_keep_line_number(true);
 		if(b.getMethod().getDeclaration().toString().contains("sourceToSink"))
@@ -56,23 +57,23 @@ public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowA
 			Unit head = it1.next();
 			System.out.println("------Head----of------is-----"+head);
 			nodeNumber++;
-			nodesMap.put(head,nodeNumber);
-			nodes[nodeNumber-1]= new Node(head.toString(), nodeNumber);
-			Node node = new Node(head.toString(), nodeNumber);
+			//nodesMap.put(head,nodeNumber);
+			//nodes[nodeNumber-1]= new Node(head.toString(), nodeNumber);
+			Node node = new Node(head, nodeNumber);
 			listNodes.add(node);
 			//System.out.println("++++++++++++++++++++++++++++++++++++++++++"+nodes[0].getLabel());
 			traverseUnits(head);
 		}
-		Set s = edgesMap.entrySet();
-		Iterator i = s.iterator();
-		while(i.hasNext())
-		{
-			Map.Entry m = (Map.Entry)i.next();
-			System.out.println(m.getKey()+"----->"+m.getValue());
-		}
-		}
-		listEdges1.add(nodesMap);
-		listEdges1.add(edgesMap);
+//		Set s = edgesMap.entrySet();
+//		Iterator i = s.iterator();
+//		while(i.hasNext())
+//		{
+//			Map.Entry m = (Map.Entry)i.next();
+//			System.out.println(m.getKey()+"----->"+m.getValue());
+//		}
+//		
+
+		
 		Iterator<Node> nodeIterator = listNodes.iterator();
 		while(nodeIterator.hasNext())
 		{
@@ -86,8 +87,10 @@ public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowA
 			Edge edge = edgeIterator.next();
 			System.out.println("Edge id is "+edge.getId()+" Edge source is "+edge.getSource().id+" Edge destination is "+edge.getDestination().id);
 		}
-		g.edgesMap=edgesMap;
-		g.nodesMap=nodesMap;
+		g.listEdges = listEdges;
+		g.listNodes = listNodes;
+
+	}
 }
 	public static void traverseUnits(Unit currentNode)
 	{
@@ -102,26 +105,36 @@ public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowA
 			Unit temp = it.next();
 			//System.out.println(temp);
 			previousNode = temp;
-			Set s = nodesMap.entrySet();
-			Iterator i = s.iterator();
-			while(i.hasNext())
+//			Set s = nodesMap.entrySet();
+//			Iterator i = s.iterator();
+//			while(i.hasNext())
+//			{
+//				//System.out.println("Herreeeeeee");
+//				Map.Entry m = (Map.Entry)i.next();
+////				System.out.println("Unit number is "+m.getValue());
+////				System.out.println("Unit key is "+m.getKey());
+//				if(m.getKey().equals(temp))
+//				{
+//					present=true;
+//				}
+//				
+//			}
+			
+			Iterator nodesIterator = listNodes.iterator();
+			while(nodesIterator.hasNext())
 			{
-				//System.out.println("Herreeeeeee");
-				Map.Entry m = (Map.Entry)i.next();
-//				System.out.println("Unit number is "+m.getValue());
-//				System.out.println("Unit key is "+m.getKey());
-				if(m.getKey().equals(temp))
+				Node node = (Node)nodesIterator.next();
+				if(node.getLabel().equals(temp))
 				{
-					present=true;
+					present = true;
 				}
-				
 			}
 			if(!present)
 			{
 			nodeNumber++;
-			nodesMap.put(temp, nodeNumber);
-			nodes[nodeNumber-1] = new Node(temp.toString(), nodeNumber);
-			Node node = new Node(temp.toString(), nodeNumber);
+			//nodesMap.put(temp, nodeNumber);
+			//nodes[nodeNumber-1] = new Node(temp.toString(), nodeNumber);
+			Node node = new Node(temp, nodeNumber);
 			listNodes.add(node);
 			}
 //			SourceLnPosTag tag = (SourceLnPosTag)temp.getTag("SourceLnPosTag");
@@ -132,61 +145,89 @@ public class IntraproceduralAnalysis extends ForwardFlowAnalysis<Unit, Set<FlowA
 //				int line = tag.startLn();
 //				System.out.println("Line number of unit is "+line);
 //			}
-			System.out.println("Draw edge from "+ nodesMap.get(currentNode) + " to "+nodesMap.get(previousNode));
-			if(present)
-			{
-				System.out.println(present);
-			  for(int k=0; k<nodes.length;k++)
-			  {
-				  //System.out.println("--->----->----->---->----->---->"+nodes[k].getLabel());
-				  if(nodes[k].getLabel().equals(temp.toString()))
-				  {
-					  edges[edgeCount++]= new Edge(edgeCount, nodes[nodeNumber-2], nodes[k]);
-					  System.out.println(nodes[k].getId());
-					  Edge edge1 = new Edge(edgeCount, nodes[nodeNumber-2], nodes[k]);
-					  listEdges.add(edge1);
-					  break;
-				  }
-			  }
-			}
-			else
-			{
-				edges[edgeCount++]= new Edge(edgeCount, nodes[nodeNumber-2], nodes[nodeNumber-1]);
-				Edge edge1 = new Edge(edgeCount, nodes[nodeNumber-2], nodes[nodeNumber-1]);
-				listEdges.add(edge1);
-			}
+			//System.out.println("Draw edge from "+ nodesMap.get(currentNode) + " to "+nodesMap.get(previousNode));
+//			if(present)
+//			{
+//				System.out.println(present);
+////			  for(int k=0; k<nodes.length;k++)
+////			  {
+////				  //System.out.println("--->----->----->---->----->---->"+nodes[k].getLabel());
+////				  if(nodes[k].getLabel().equals(temp.toString()))
+////				  {
+////					  edges[edgeCount++]= new Edge(edgeCount, nodes[nodeNumber-2], nodes[k]);
+////					  System.out.println(nodes[k].getId());
+////					  Edge edge1 = new Edge(edgeCount, nodes[nodeNumber-2], nodes[k]);
+////					  listEdges.add(edge1);
+////					  break;
+////				  }
+////			  }
+//				
+//			}
+//			else
+//			{
+//				edges[edgeCount++]= new Edge(edgeCount, nodes[nodeNumber-2], nodes[nodeNumber-1]);
+//				Edge edge1 = new Edge(edgeCount, nodes[nodeNumber-2], nodes[nodeNumber-1]);
+//				listEdges.add(edge1);
+//			}
 			//System.out.println("Draw edge from "+ set.get(currentNode) + " to "+set.get(previousNode));
 			//edges[edgeCount] = new Edge(++edgeCount, nodes[nodeCount-1], nodes[nodeCount]);
 			//set1.put(set.get(currentNode), set.get(previousNode));
-			Set s1 = edgesMap.entrySet();
-			Iterator i1 = s1.iterator();
-			while(i1.hasNext())
+//			Set s1 = edgesMap.entrySet();
+//			Iterator i1 = s1.iterator();
+//			while(i1.hasNext())
+//			{
+//				Map.Entry m = (Map.Entry)i1.next();
+//				if(m.getKey().equals(nodesMap.get(currentNode)))
+//				{
+//					edge=true;
+//				}
+//				
+//			}
+//			Iterator edgeIterator = listEdges.iterator();
+//			while(edgeIterator.hasNext())
+//			{
+//				Edge tempEdge = (Edge)edgeIterator.next();
+//				if(tempEdge.source.equals(currentNode))
+//				{
+//					edge = true;
+//				}
+//			}
+//			if(!edge)
+//			{
+//				edgeNumber++;
+//				List<Integer> list = new ArrayList<>();
+//				list.add(nodesMap.get(previousNode));
+//				
+//				edgesMap.put(nodesMap.get(currentNode), list);
+//			}
+//			if(edge)
+//			{				
+//				List<Integer> list = edgesMap.get(nodesMap.get(currentNode));
+//				list.add(nodesMap.get(previousNode));
+//				edgesMap.put(nodesMap.get(currentNode), list);
+//			}
+			Node source=null, destination=null;
+			Iterator it1 = listNodes.iterator();
+			while(it1.hasNext())
 			{
-				Map.Entry m = (Map.Entry)i1.next();
-				if(m.getKey().equals(nodesMap.get(currentNode)))
+				Node node = (Node)it1.next();
+				if(node.getLabel().equals(currentNode))
 				{
-					edge=true;
+					//System.out.println("Herererererere");
+					source = node;
 				}
-				
+				if(node.getLabel().equals(temp))
+				{
+					//System.out.println("Herererererere");
+					destination = node;
+				}
 			}
-			if(!edge)
-			{
-				List<Integer> list = new ArrayList<>();
-				list.add(nodesMap.get(previousNode));
-				edgesMap.put(nodesMap.get(currentNode), list);
-			}
-			if(edge)
-			{				
-				List<Integer> list = edgesMap.get(nodesMap.get(currentNode));
-				list.add(nodesMap.get(previousNode));
-				edgesMap.put(nodesMap.get(currentNode), list);
-			}
+			edgeNumber++;
+			Edge edgeEntry = new Edge(edgeNumber,source , destination);
+			listEdges.add(edgeEntry);
 			traverseUnits(temp);
 		}
-		if(previousNode!=null)
-		{
 		
-		}
 	}
 
 	@Override
