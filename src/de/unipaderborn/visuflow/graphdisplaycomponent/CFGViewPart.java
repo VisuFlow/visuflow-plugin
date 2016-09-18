2154609420aaac6580cae85ecfbd64d5622c5a3a
Henrik Niehaus <henrik.niehaus@gmx.de>
2016-09-18 15:57:46 +0200

    Moved GraphStream view to ViewPart. Introduced perspective for the Visuflow plug-in

---
+++ src/de/unipaderborn/visuflow/graphdisplaycomponent/CFGViewPart.java
@@ -0,0 +1,44 @@
+package de.unipaderborn.visuflow.graphdisplaycomponent;
+
+import java.awt.Frame;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CFGViewPart extends ViewPart {
+
+    @Override
+    public void createPartControl(Composite parent) {
+        createGraphComposite(parent);
+    }
+
+    private void createGraphComposite(Composite parent) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Composite composite = new Composite(parent, SWT.EMBEDDED | SWT.NO_BACKGROUND);
+        GraphManager manager = new GraphManager("test", "url('file:../../styles/stylesheet.css')");
+        Thread t = new Thread(manager);
+        t.start();
+
+        Frame frame = SWT_AWT.new_Frame(composite);
+        frame.add(manager.getApplet());
+        frame.pack();
+    }
+
+    @Override
+    public void setFocus() {
+        System.out.println(getClass().getName() + " Set Focus");
+    }
+
+}
---
+++ src/de/unipaderborn/visuflow/ui/VisuflowEditorPerspective.java
@@ -0,0 +1,55 @@
+package de.unipaderborn.visuflow.ui;
+
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class VisuflowEditorPerspective implements IPerspectiveFactory {
+
+    private static final String ID_FOLDER_JIMPLE = "perspective.folder.jimple";
+    private static final String ID_FOLDER_GRAPH = "perspective.folder.graph";
+    private static final String ID_FOLDER_CONSOLE = "perspective.folder.console";
+    private static final String ID_FOLDER_DEBUG = "perspective.folder.debug";
+
+    private static final String ID_GRAPH_VIEW = "de.unipaderborn.visuflow.graphdisplaycomponent.CFGViewPart";
+    private static final String ID_JIMPLE_VIEW = "graphstreamfeasibility.views.SampleView";
+    private static final String ID_CONSOLE_VIEW = "org.eclipse.ui.console.ConsoleView";
+
+    @Override
+    public void createInitialLayout(IPageLayout layout) {
+        IFolderLayout consoleFolder = layout.createFolder(ID_FOLDER_CONSOLE, IPageLayout.BOTTOM, (float) 0.66, layout.getEditorArea());
+        consoleFolder.addView(ID_CONSOLE_VIEW);
+
+        IFolderLayout debugFolder = layout.createFolder(ID_FOLDER_DEBUG, IPageLayout.RIGHT, (float) 0.5, ID_FOLDER_CONSOLE);
+        debugFolder.addView(IDebugUIConstants.ID_VARIABLE_VIEW);
+        debugFolder.addView(IDebugUIConstants.ID_BREAKPOINT_VIEW);
+        debugFolder.addPlaceholder(IDebugUIConstants.ID_EXPRESSION_VIEW);
+        debugFolder.addPlaceholder(IDebugUIConstants.ID_REGISTER_VIEW);
+
+        IFolderLayout jimpleFolder = layout.createFolder(ID_FOLDER_JIMPLE, IPageLayout.RIGHT, (float) 0.33, layout.getEditorArea());
+        jimpleFolder.addView(ID_JIMPLE_VIEW);
+
+        IFolderLayout graphFolder = layout.createFolder(ID_FOLDER_GRAPH, IPageLayout.RIGHT, (float) 0.33, ID_FOLDER_JIMPLE);
+        graphFolder.addView(ID_GRAPH_VIEW);
+
+        layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
+        layout.addActionSet(IDebugUIConstants.DEBUG_ACTION_SET);
+
+        setContentsOfShowViewMenu(layout);
+    }
+
+    /**
+     * Sets the initial contents of the "Show View" menu.
+     */
+    protected void setContentsOfShowViewMenu(IPageLayout layout) {
+        layout.addShowViewShortcut(IDebugUIConstants.ID_DEBUG_VIEW);
+        layout.addShowViewShortcut(IDebugUIConstants.ID_VARIABLE_VIEW);
+        layout.addShowViewShortcut(IDebugUIConstants.ID_BREAKPOINT_VIEW);
+        layout.addShowViewShortcut(IDebugUIConstants.ID_EXPRESSION_VIEW);
+        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+        layout.addShowViewShortcut(ID_CONSOLE_VIEW);
+        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+    }
+
+}
