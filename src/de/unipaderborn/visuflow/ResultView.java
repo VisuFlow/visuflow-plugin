e2ed6343d554a52f2338a4129acadb427c86c581
Shashank B S <bsshashank91@gmail.com>
2016-10-16 23:29:14 +0200

    Updated ResultView to make use of JFace to display units and added flow set info

--- src/de/unipaderborn/visuflow/ResultView.java
+++ src/de/unipaderborn/visuflow/ResultView.java
@@ -1,13 +1,20 @@
 package de.unipaderborn.visuflow;
 
-import java.awt.Frame;
 import java.util.List;
 
 import javax.swing.JTable;
-
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 import de.unipaderborn.visuflow.model.DataModel;
@@ -22,6 +29,7 @@ public class ResultView extends ViewPart {
 	String[] columnNames = {"Unit","Statement Type"};
 	Object[][] data = new Object[10000][2];
 	private List<VFClass> analysisData;
+	private TableViewer viewer;
 
 	public ResultView() {
 		// TODO Auto-generated constructor stub
@@ -40,28 +48,83 @@ public class ResultView extends ViewPart {
 		}
 	}
 
-	public void setColumnNames(String[] columnNames) {
-		this.columnNames = columnNames;
-	}
-
-	public void setData(Object[][] data) {
-		this.data = data;
-	}
-
 	@Override
 	public void createPartControl(Composite parent) {
-		Composite composite = new Composite(parent, SWT.NO_BACKGROUND | SWT.EMBEDDED);
-		Frame frame = SWT_AWT.new_Frame(composite);
-
-		nodeTable = new JTable(data, columnNames);
-		frame.add(nodeTable);
-		frame.pack();
+		GridLayout layout = new GridLayout(2, false);
+		parent.setLayout(layout);
+		Label searchLabel = new Label(parent, SWT.NONE);
+		searchLabel.setText("Search: ");
+		final Text searchText = new Text(parent, SWT.BORDER | SWT.SEARCH);
+		searchText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+				| GridData.HORIZONTAL_ALIGN_FILL));
+		createViewer(parent);
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-
+		viewer.getControl().setFocus();
 	}
 
+	private void createViewer(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+				| SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+		createColumns(parent, viewer);
+		final Table table = viewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+
+		viewer.setContentProvider(new ArrayContentProvider());
+		// get the content for the viewer, setInput will call getElements in the
+		// contentProvider
+		viewer.setInput(ModelProvider.INSTANCE.getUnits());
+		// make the selection available to other views
+		getSite().setSelectionProvider(viewer);
+		// set the sorter for the table
+
+		// define layout for the viewer
+		GridData gridData = new GridData();
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.horizontalSpan = 2;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		viewer.getControl().setLayoutData(gridData);
+	}
+
+	private void createColumns(final Composite parent, final TableViewer viewer) {
+		String[] titles = { "Unit", "Unit Type"};
+		int[] bounds = { 100, 100, 100, 100 };
+
+		// first column is for the first name
+		TableViewerColumn col = createTableViewerColumn(titles[0], bounds[0], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				VFUnit unit = (VFUnit) element;
+				return unit.getUnit().toString();
+			}
+		});
+
+		// second column is for the last name
+		col = createTableViewerColumn(titles[1], bounds[1], 1);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				VFUnit unit = (VFUnit) element;
+				return unit.getUnit().getClass().getName();
+			}
+		});
+	}
+
+	private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer,
+				SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		return viewerColumn;
+	}
 }
