579885474d7f2617af15ac0ca2985f4ae4459fc9
karthikraobr <karthik.0703@gmail.com>
2016-10-29 13:47:23 +0200

    Jimple Breakpoint WIP

--- src/de/unipaderborn/visuflow/debug/handlers/JimpleBreakPointHandler.java
+++ src/de/unipaderborn/visuflow/debug/handlers/JimpleBreakPointHandler.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -18,6 +23,7 @@ import org.eclipse.ui.texteditor.ITextEditor;
 import de.unipaderborn.visuflow.model.DataModel;
 import de.unipaderborn.visuflow.model.VFMethod;
 import de.unipaderborn.visuflow.util.ServiceUtil;
+import scala.reflect.macros.internal.macroImpl;
 
 import org.eclipse.jface.dialogs.*;
 
@@ -31,9 +37,12 @@ public class JimpleBreakPointHandler extends AbstractHandler {
 		for (VFMethod vfMethod : methods) {
 			System.out.println(vfMethod.getBody());
 		}
-		
+
 		IEditorPart part =PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		IFileEditorInput input = (IFileEditorInput)part.getEditorInput() ;
+		IFile file = input.getFile();
+		IResource res = (IResource) file;
 
 		if (part instanceof ITextEditor)
 		{
@@ -47,10 +56,39 @@ public class JimpleBreakPointHandler extends AbstractHandler {
 				int length=document.getLineInformation(lineNumber).getLength();
 				int actualLineNumber = lineNumber+1;
 				String content =document.get(offset,length).trim();
+
+				IMarker[] problems = null;
+				int depth = IResource.DEPTH_ZERO;
+				problems = res.findMarkers(IMarker.TASK, true, depth);
+				Boolean markerPresent = false;
+				for (IMarker item : problems) {
+					int markerLineNmber = (int) item.getAttribute(IMarker.LINE_NUMBER);
+					if(markerLineNmber == actualLineNumber){
+						markerPresent=true;
+						item.delete();
+						MessageDialog.openInformation(window.getShell(), "Debugger deleted at line: "+actualLineNumber,content);
+					}
+				}
+
+				if(!markerPresent){
+
+					IMarker m = res.createMarker(IMarker.TASK);
+					m.setAttribute(IMarker.LINE_NUMBER,actualLineNumber);
+					m.setAttribute(IMarker.MESSAGE, content);
+					m.setAttribute(IMarker.TEXT, "Jimple Breakpoint");
+					m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+					m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
+					MessageDialog.openInformation(window.getShell(), "Debugger set at line: "+actualLineNumber,content);
+				}
+
+
 				System.out.printf("Line Number:%d\n",(lineNumber+1));
 				System.out.printf("The contents of the line :   %s",content);
-				MessageDialog.openInformation(window.getShell(), "Set debugger at line: "+actualLineNumber,content);
+
 			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (CoreException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
