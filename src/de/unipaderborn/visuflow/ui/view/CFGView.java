db2d1747522890619c134e08f834858aca0058ed
Shashank B S <bsshashank91@gmail.com>
2016-10-31 13:03:46 +0100

    Changed graph name

--- src/de/unipaderborn/visuflow/ui/view/CFGView.java
+++ src/de/unipaderborn/visuflow/ui/view/CFGView.java
@@ -29,7 +29,7 @@ public class CFGView extends ViewPart {
 		}
 
 		Composite composite = new Composite(parent, SWT.EMBEDDED | SWT.NO_BACKGROUND);
-		GraphManager manager = new GraphManager("test", "url('file:styles/stylesheet.css')");
+		GraphManager manager = new GraphManager("VisuFlow Graph", "url('file:styles/stylesheet.css')");
 		Thread t = new Thread(manager);
 		t.start();
 
