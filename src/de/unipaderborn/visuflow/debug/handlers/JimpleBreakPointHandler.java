396c6a0fe74a352fbd21ec8b9a3cff39c25b929d
karthikraobr <karthik.0703@gmail.com>
2016-11-05 23:15:53 +0100

    Added function to filter out selected unit from jimple editor

--- src/de/unipaderborn/visuflow/debug/handlers/JimpleBreakPointHandler.java
+++ src/de/unipaderborn/visuflow/debug/handlers/JimpleBreakPointHandler.java
@@ -1,7 +1,7 @@
 package de.unipaderborn.visuflow.debug.handlers;
 
-import java.util.List;
 
+import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -21,13 +21,14 @@ import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 import de.unipaderborn.visuflow.model.DataModel;
+import de.unipaderborn.visuflow.model.VFClass;
 import de.unipaderborn.visuflow.model.VFMethod;
+import de.unipaderborn.visuflow.model.VFUnit;
 import de.unipaderborn.visuflow.util.ServiceUtil;
 
 import org.eclipse.jface.dialogs.*;
 
 public class JimpleBreakPointHandler extends AbstractHandler {
-
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -37,60 +38,86 @@ public class JimpleBreakPointHandler extends AbstractHandler {
 			System.out.println(vfMethod.getBody());
 		}
 
-		IEditorPart part =PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		IEditorPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-		IFileEditorInput input = (IFileEditorInput)part.getEditorInput() ;
+		IFileEditorInput input = (IFileEditorInput) part.getEditorInput();
 		IFile file = input.getFile();
 		IResource res = (IResource) file;
 
-		if (part instanceof ITextEditor)
-		{
+		if (part instanceof ITextEditor) {
 			final ITextEditor editor = (ITextEditor) part;
 			IVerticalRulerInfo ruleInfo = editor.getAdapter(IVerticalRulerInfo.class);
 			IDocumentProvider provider = editor.getDocumentProvider();
 			IDocument document = provider.getDocument(editor.getEditorInput());
 			try {
 				int lineNumber = ruleInfo.getLineOfLastMouseButtonActivity();
-				int offset=document.getLineOffset(lineNumber);
-				int length=document.getLineInformation(lineNumber).getLength();
-				int actualLineNumber = lineNumber+1;
-				String content =document.get(offset,length).trim();
+				int offset = document.getLineOffset(lineNumber);
+				int length = document.getLineInformation(lineNumber).getLength();
+				int actualLineNumber = lineNumber + 1;
+				String content = document.get(offset, length).trim();
+				String className = file.getName().substring(0, file.getName().lastIndexOf('.'));
+				VFUnit resultantUnit = getSelectedUnit(content.trim().substring(0, content.length() - 1), className);
+				if (resultantUnit == null) {
+					MessageDialog.openInformation(window.getShell(), "Breakpoint could not be placed",
+							"Error in inserting breakpoint");
+				} else {
+					IMarker[] problems = null;
+					int depth = IResource.DEPTH_ZERO;
+					problems = res.findMarkers("de.uni-paderborn.visuflow.plugin.JimpleBreakPointMarker", true, depth);
 
-				IMarker[] problems = null;
-				int depth = IResource.DEPTH_ZERO;
-				problems = res.findMarkers("de.uni-paderborn.visuflow.plugin.JimpleBreakPointMarker", true, depth);
-				
-				Boolean markerPresent = false;
-				for (IMarker item : problems) {
-					int markerLineNmber = (int) item.getAttribute(IMarker.LINE_NUMBER);
-					if(markerLineNmber == actualLineNumber){
-						markerPresent=true;
-						item.delete();
-						MessageDialog.openInformation(window.getShell(), "Debugger deleted at line: "+actualLineNumber,content);
+					Boolean markerPresent = false;
+					for (IMarker item : problems) {
+						int markerLineNmber = (int) item.getAttribute(IMarker.LINE_NUMBER);
+						if (markerLineNmber == actualLineNumber) {
+							markerPresent = true;
+							item.delete();
+							MessageDialog.openInformation(window.getShell(),
+									"Debugger deleted at line: " + actualLineNumber, content);
+						}
 					}
+
+					if (!markerPresent) {
+
+						IMarker m = res.createMarker("de.uni-paderborn.visuflow.plugin.JimpleBreakPointMarker");
+						m.setAttribute(IMarker.LINE_NUMBER, actualLineNumber);
+						m.setAttribute(IMarker.MESSAGE, content);
+						m.setAttribute(IMarker.TEXT, "Jimple Breakpoint");
+						m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+						m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
+						MessageDialog.openInformation(window.getShell(), "Debugger set at line: " + actualLineNumber,
+								content);
+					}
+
+					System.out.printf("Line Number:%d\n", (lineNumber + 1));
+					System.out.printf("The contents of the line :   %s", content);
+
+					System.out.println();
 				}
-
-				if(!markerPresent){
-
-					IMarker m = res.createMarker("de.uni-paderborn.visuflow.plugin.JimpleBreakPointMarker");
-					m.setAttribute(IMarker.LINE_NUMBER,actualLineNumber);
-					m.setAttribute(IMarker.MESSAGE, content);
-					m.setAttribute(IMarker.TEXT, "Jimple Breakpoint");
-					m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
-					m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
-					MessageDialog.openInformation(window.getShell(), "Debugger set at line: "+actualLineNumber,content);
-				}
-
-
-				System.out.printf("Line Number:%d\n",(lineNumber+1));
-				System.out.printf("The contents of the line :   %s",content);
-
 			} catch (BadLocationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (CoreException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	private VFUnit getSelectedUnit(String content, String className) {
+		DataModel dataModel = ServiceUtil.getService(DataModel.class);
+		// VFClass
+		// vfClass=dataModel.listClasses().stream().filter(x->x.getSootClass().getName()==className).collect(Collectors.toList()).get(0);
+		for (VFClass vfClass : dataModel.listClasses()) {
+			if (vfClass.getSootClass().getName().equals(className)) {
+				List<VFMethod> vfMethods = vfClass.getMethods();
+				for (VFMethod method : vfMethods) {
+					for (VFUnit unit : method.getUnits()) {
+						if (unit.getUnit().toString().trim().equals(content)) {
+							return unit;
+						}
+					}
+				}
 			}
 		}
 		return null;
