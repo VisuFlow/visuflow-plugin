8a287f9cb827b71e9f4e803a30170a4a69df257c
Zafar Syed <zafar.habeeb1989@gmail.com>
2016-11-14 10:40:34 +0100

    Added Wizard page to add Target folder

---
+++ src/de/unipaderborn/visuflow/wizard/WizardHandler.java
@@ -0,0 +1,202 @@
+package de.unipaderborn.visuflow.wizard;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWizard;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+
+public class WizardHandler extends Wizard implements INewWizard {
+
+	private WizardPageHandler page;
+	private ISelection selection;
+
+	/**
+	 * Constructor for SampleNewWizard.
+	 */
+	public WizardHandler() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+	
+	/**
+	 * Adding the page to the wizard.
+	 */
+
+	public void addPages() {
+		page = new WizardPageHandler(selection);
+		addPage(page);
+	}
+
+	/**
+	 * This method is called when 'Finish' button is pressed in
+	 * the wizard. We will create an operation and run it
+	 * using wizard as execution context.
+	 */
+	public boolean performFinish() {
+		final String containerName = page.getContainerName().get("ProjectPath");
+		final String containerName1 = page.getContainerName().get("TargetPath");
+		final String fileName = page.getFileName();
+		System.out.println("Container name is "+containerName);
+		System.out.println("Container name is "+containerName1);
+		System.out.println("File name is "+fileName);
+		IRunnableWithProgress op = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				try {
+					doFinish(containerName, containerName1, fileName, monitor);
+				} catch (CoreException e) {
+					throw new InvocationTargetException(e);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					monitor.done();
+				}
+			}
+		};
+		try {
+			getContainer().run(true, false, op);
+		} catch (InterruptedException e) {
+			return false;
+		} catch (InvocationTargetException e) {
+			Throwable realException = e.getTargetException();
+			MessageDialog.openError(getShell(), "Error", realException.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * The worker method. It will find the container, create the
+	 * file if missing or just replace its contents, and open
+	 * the editor on the newly created file.
+	 * @throws FileNotFoundException 
+	 */
+
+	private void doFinish(
+		String containerName,
+		String containerName1,
+		String fileName,
+		IProgressMonitor monitor)
+		throws CoreException, FileNotFoundException {
+		// create a sample file
+		monitor.beginTask("Creating " + fileName, 2);
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		//IResource resource1 = root.findMember(new Path(containerName1));
+		File fileDir = new File(containerName1);
+		System.out.println("file directory is "+fileDir.getName());
+		System.out.println("File directory is directory "+fileDir.isDirectory());
+		
+		if (!resource.exists() || !(resource instanceof IContainer)) {
+			throwCoreException("Container \"" + containerName + "\" does not exist.");
+		}
+		
+		IContainer container = (IContainer) resource;
+		IFolder target = container.getFolder(new Path(fileDir.getName()));
+		target.create(true, true, monitor);
+		System.out.println("Target destination "+target.getFullPath().toString());
+		IResource r = root.findMember(new Path(target.getFullPath().toString()));
+		IContainer c = (IContainer) r;
+		System.out.println("Container c is "+c);
+		copyFiles(fileDir, c, monitor);
+		final IFile file = container.getFile(new Path(fileName));		
+		try {
+			InputStream stream = openContentStream();
+			if (file.exists()) {
+				file.setContents(stream, true, true, monitor);
+			} else {
+				file.create(stream, true, monitor);
+			}
+			stream.close();
+		} catch (IOException e) {
+		}
+		
+//		IContainer container1 = (IContainer) resource1;
+//		final IFolder folder = container.getFolder(new Path("Test"));
+//		if(!folder.exists())
+//		{
+//			folder.create(1, true, monitor);
+//		}
+		monitor.worked(1);
+		monitor.setTaskName("Opening file for editing...");
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page =
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+		monitor.worked(1);
+	}
+	
+	/**
+	 * We will initialize file contents with a sample text.
+	 * @throws CoreException 
+	 * @throws FileNotFoundException 
+	 */
+	
+	void copyFiles (File srcFolder, IContainer destFolder, IProgressMonitor monitor) throws CoreException, FileNotFoundException {
+	    for (File f: srcFolder.listFiles()) {
+	        if (f.isDirectory()) {
+	            IFolder newFolder = destFolder.getFolder(new Path(f.getName()));
+	            newFolder.create(true, true, monitor);
+	            copyFiles(f, newFolder, monitor);
+	        } else {
+	            IFile newFile = destFolder.getFile(new Path(f.getName()));
+	            newFile.create(new FileInputStream(f), true, monitor);
+	        }
+	    }
+	}
+	
+	private InputStream openContentStream() {
+		String contents =
+			"This is the initial file contents for *.java file that should be word-sorted in the Preview page of the multi-page editor";
+		return new ByteArrayInputStream(contents.getBytes());
+	}
+
+	private void throwCoreException(String message) throws CoreException {
+		IStatus status =
+			new Status(IStatus.ERROR, "TestPlugIn", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
+
+	/**
+	 * We will accept the selection in the workbench to see if
+	 * we can initialize from it.
+	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+	}
+
+}
---
+++ src/de/unipaderborn/visuflow/wizard/WizardPageHandler.java
@@ -0,0 +1,227 @@
+package de.unipaderborn.visuflow.wizard;
+
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+public class WizardPageHandler extends WizardPage {
+	private Text containerText,containerText1;
+
+	private Text fileText;
+
+	private ISelection selection;
+
+	/**
+	 * Constructor for SampleNewWizardPage.
+	 * 
+	 * @param pageName
+	 */
+	public WizardPageHandler(ISelection selection) {
+		super("wizardPage");
+		setTitle("Multi-page Editor File");
+		setDescription("This wizard creates a new file with *.java extension that can be opened by a multi-page editor.");
+		this.selection = selection;
+	}
+
+	/**
+	 * @see IDialogPage#createControl(Composite)
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		Label label = new Label(container, SWT.NULL);
+		label.setText("&Container:");
+
+		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		containerText.setLayoutData(gd);
+		containerText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
+		label = new Label(container, SWT.NULL);
+		label.setText("&File name:");
+
+		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		fileText.setLayoutData(gd);
+		fileText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+		
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		Button button1 = new Button(container, SWT.MULTI);
+		button1.setText("Soot");
+		button1.setLayoutData(gd);
+		Button button2 = new Button(container, SWT.RADIO);
+		button2.setText("Heros");
+		button2.setLayoutData(gd);
+		Button button3 = new Button(container, SWT.RADIO);
+		button3.setText("Vasco");
+		button3.setLayoutData(gd);
+		
+		
+		Label labelFile = new Label(container, SWT.NULL);
+		labelFile.setText("Choose Folder:");
+
+		containerText1 = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		containerText1.setLayoutData(gd);
+		containerText1.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button buttonFile = new Button(container, SWT.PUSH);
+		buttonFile.setText("Browse...");
+		buttonFile.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse1();
+			}
+		});
+		
+		initialize();
+		dialogChanged();
+		setControl(container);
+	}
+
+	/**
+	 * Tests if the current workbench selection is a suitable container to use.
+	 */
+
+	private void initialize() {
+		if (selection != null && selection.isEmpty() == false
+				&& selection instanceof IStructuredSelection) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1)
+				return;
+			Object obj = ssel.getFirstElement();
+			if (obj instanceof IResource) {
+				IContainer container;
+				if (obj instanceof IContainer)
+					container = (IContainer) obj;
+				else
+					container = ((IResource) obj).getParent();
+				containerText.setText(container.getFullPath().toString());
+			}
+		}
+		fileText.setText("new_file.java");
+	}
+
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+
+	private void handleBrowse() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select new file container");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				containerText.setText(((Path) result[0]).toString());
+			}
+		}
+	}
+	
+	
+	private void handleBrowse1() {
+		DirectoryDialog dialog = new DirectoryDialog(getShell(), SWT.OPEN);
+		dialog.setMessage("Select Folder");
+		dialog.setFilterPath("C:\\");
+		String result = dialog.open();
+		containerText1.setText(result);
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+
+	private void dialogChanged() {
+		IResource container = ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(new Path(getContainerName().get("ProjectPath")));
+		String fileName = getFileName();
+
+		if (getContainerName().get("ProjectPath").length() == 0) {
+			updateStatus("File container must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+			updateStatus("File container must exist");
+			return;
+		}
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
+			return;
+		}
+		if (fileName.length() == 0) {
+			updateStatus("File name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("File name must be valid");
+			return;
+		}
+		int dotLoc = fileName.lastIndexOf('.');
+		if (dotLoc != -1) {
+			String ext = fileName.substring(dotLoc + 1);
+			if (ext.equalsIgnoreCase("java") == false) {
+				updateStatus("File extension must be \"java\"");
+				return;
+			}
+		}
+		updateStatus(null);
+	}
+
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public HashMap<String, String> getContainerName() {
+		HashMap<String, String> containerMap = new HashMap<>();
+		containerMap.put("ProjectPath", containerText.getText());
+		containerMap.put("TargetPath", containerText1.getText());
+		return containerMap;
+	}
+
+	public String getFileName() {
+		return fileText.getText();
+	}
+}
