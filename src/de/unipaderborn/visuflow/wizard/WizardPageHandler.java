30d5949c9460074e114d1a296dd211156aeef4dc
Shashank B S <bsshashank91@gmail.com>
2016-12-03 15:28:13 +0100

    Resolved warnings

--- src/de/unipaderborn/visuflow/wizard/WizardPageHandler.java
+++ src/de/unipaderborn/visuflow/wizard/WizardPageHandler.java
@@ -18,7 +18,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ContainerSelectionDialog;
