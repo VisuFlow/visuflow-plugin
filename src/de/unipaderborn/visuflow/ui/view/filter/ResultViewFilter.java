78c090c0c3aebe3febcdb3918585b02c96059c1e
Henrik Niehaus <henni@upb.de>
2017-01-18 17:07:51 +0100

    ResultView now displays the analysis results. The results are also included in the search results.

--- src/de/unipaderborn/visuflow/ui/view/filter/ResultViewFilter.java
+++ src/de/unipaderborn/visuflow/ui/view/filter/ResultViewFilter.java
@@ -3,6 +3,8 @@ package de.unipaderborn.visuflow.ui.view.filter;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
+import com.google.common.base.Optional;
+
 import de.unipaderborn.visuflow.model.VFUnit;
 
 public class ResultViewFilter extends ViewerFilter {
@@ -10,8 +12,7 @@ public class ResultViewFilter extends ViewerFilter {
 	private String searchString;
 
 	public void setSearchText(String s) {
-		// ensure that the value can be used for matching
-		this.searchString = ".*" + s + ".*";
+		this.searchString = s;
 	}
 
 	@Override
@@ -20,13 +21,27 @@ public class ResultViewFilter extends ViewerFilter {
 			return true;
 		}
 		VFUnit unit = (VFUnit) element;
-		if (unit.getUnit().toString().matches(searchString)) {
+		if (contains(unit.getUnit().toString(), searchString)) {
 			return true;
 		}
-		if (unit.getUnit().getClass().getName().matches(searchString)) {
+		if (contains(unit.getUnit().getClass().getName(), searchString)) {
+			return true;
+		}
+		
+		String inset = Optional.fromNullable(unit.getInSet()).or("n/a").toString();
+		if(contains(inset, searchString)) {
+			return true;
+		}
+		
+		String outset = Optional.fromNullable(unit.getInSet()).or("n/a").toString();
+		if(contains(outset, searchString)) {
 			return true;
 		}
 
 		return false;
+	}
+	
+	private boolean contains(String haystack, String needle) {
+		return haystack.toLowerCase().contains(needle.toLowerCase());
 	}
 }
