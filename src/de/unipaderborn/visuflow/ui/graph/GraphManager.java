61d0bf49bd63d9712fde84f9364f20ddc681f71e
Shashank B S <bsshashank91@gmail.com>
2017-03-27 12:02:05 +0200

    Added javadoc to the HierarchicalLayout.java file

--- src/de/unipaderborn/visuflow/ui/graph/GraphManager.java
+++ src/de/unipaderborn/visuflow/ui/graph/GraphManager.java
@@ -29,7 +29,6 @@ import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
